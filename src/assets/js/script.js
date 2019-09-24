0d009ffc60dae5245f625736f97b84aa65be1649
shubhitaran91 <shubhitaran@gmail.com>
2019-09-24 16:16:44 +0530

    update

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,7 +5,7 @@ function myFunction() {
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
+      td = tr[i].getElementsByTagName("td")[1];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -21,3 +21,4 @@ function myFunction() {
     $('#myModal').modal('show');
   }
 
+ 
