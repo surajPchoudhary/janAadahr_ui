7ed7a94b1f8201cf439150bba418c2a4f64a01d0
harinishree <harinishree.muniraj@rapidqube.com>
2019-10-04 18:26:32 +0530

    ui testing bugs removed

--- src/app/views/transaction-id/transaction-id.component.ts
+++ src/app/views/transaction-id/transaction-id.component.ts
@@ -8,9 +8,13 @@ import { UserService } from 'src/app/user.service';
   styleUrls: ['./transaction-id.component.scss']
 })
 export class TransactionIdComponent implements OnInit {
+public gh1:any
+public gh:any
 public mem_id:any
 public familyid:any
+public familyInfo:any
 name:any
+HOFname:any
 t1:any
   constructor(public route:ActivatedRoute,public service:UserService) { }
 
@@ -20,10 +24,52 @@ this.mem_id=this.route.snapshot.queryParamMap.get('mem_id')
 this.name=this.route.snapshot.queryParamMap.get('name')
 this.service.getFamilyInfo(this.familyid).subscribe(data=>{
 this.t1=data
+var len=this.t1.length
+    for(var i=0;i<len;i++){
+    console.log(this.t1[i].data.MEM_ID,"============>")
+    if(this.t1[i].data.MEM_ID==this.mem_id){
+      
+     
+  
+    console.log(this.t1[i],"***********")
+    this.familyInfo=this.t1[i]
+    this.gh1=this.familyInfo.data
 
-console.log("transaction",this.t1)
-})
 
+     if(this.t1[i].data.HOF==='HOF'){
+      this.HOFname=this.gh1.Name
+      console.log("HOF name",this.HOFname);}
+    
+    console.log("particular member",this.familyInfo.data)
+    return this.t1[i]
+    }
   }
 
+})
+
+  
 }
+  
+  view(memberid){
+    console.log("memberid",memberid)
+    var len=this.t1.length
+    for(var i=0;i<len;i++){
+    console.log(this.t1[i].data.MEM_ID,"============>")
+    if(this.t1[i].data.MEM_ID==memberid){
+    console.log(this.t1[i],"***********")
+    this.familyInfo=this.t1[i]
+    this.gh=this.familyInfo.data
+    console.log("familyIngo",this.familyInfo.data)
+    return this.t1[i]
+    }
+}
+
+  }
+}
+
+
+
+
+
+
+
